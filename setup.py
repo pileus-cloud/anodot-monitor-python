83062cba36c22a528183df8ec883e7b884160c1b
Alexander <shereshevsky@gmail.com>
2023-08-29 13:10:36 +0300

    init

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='anodot-monitor',
-    version="0.6",
+    version="0.7",
     description='Anodot Monitoring',
     author='Alexander Shereshevsky',
     author_email='shereshevsky@gmail.com',
