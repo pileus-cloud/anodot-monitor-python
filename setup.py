5feaa1ba23d0e2fbbbba6d40089d59d898c62dd2
Alexander <shereshevsky@gmail.com>
2023-09-21 12:49:07 +0300

    version update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='anodot-monitor',
-    version="0.7",
+    version="0.8",
     description='Anodot Monitoring',
     author='Alexander Shereshevsky',
     author_email='shereshevsky@gmail.com',
